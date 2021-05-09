4a3b856ffc91d5268c86d3692143c1a12b954fec
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-05-09 00:52:35 -0500

    api name regex

--- src/main/java/com/usmp/controller/PaymentController.java
+++ src/main/java/com/usmp/controller/PaymentController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/payment")
+@RequestMapping("/api/payment")
 public class PaymentController {
 
     private CustomerService customerService;
